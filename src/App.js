9bb0338cbc78a239292331175623d67643b59933
neilbenjamin <neilbenjaminmusic@gmail.com>
2023-09-06 21:58:01 +0200

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 //Main App to index.js
-import './App.css';
-import LayoutComponent from "./layout.js"
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "./App.css";
+import LayoutComponent from "./layout.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <LayoutComponent/>
+        <LayoutComponent />
       </header>
     </div>
   );
