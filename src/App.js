5ae131a43bf6ba0c5fe134e091dccc2bc2c22bb4
neilbenjamin <neilbenjaminmusic@gmail.com>
2023-09-06 21:53:56 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+//Main App to index.js
 import './App.css';
+import LayoutComponent from "./layout.js"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LayoutComponent/>
       </header>
     </div>
   );
